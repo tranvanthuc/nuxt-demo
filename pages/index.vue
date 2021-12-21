466ae0dce525fa25f8f7d7e4bf33e235d85e4348
Thuc Tran <thuc.tv@techchain.vn>
2021-12-21 11:30:03 +0700

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  methods: {
+    async fetchPosts() {
+      this.posts = await this.$axios.$get('/posts');
+    },
+  },
+};
+</script>
