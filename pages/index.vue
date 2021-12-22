939ac6668641343a1330ff7ed69850566d2a5d0a
Thuc Tran <thuc.tv@techchain.vn>
2021-12-22 15:31:43 +0700

    add form add post

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <form @submit.prevent="handleSubmit">
+      <label for="tilte">Post</label>
+      <input type="text" name="title" v-model="form.title" ref="title" />
+      <button type="submit">Add</button>
+    </form>
     <ul>
       <li v-for="post in posts" :key="post.id">
         {{ post.title }}
@@ -14,12 +19,22 @@ export default {
   data() {
     return {
       posts: [],
+      form: {
+        title: '',
+      },
     };
   },
   mounted() {
     this.fetchPosts();
   },
   methods: {
+    async handleSubmit() {
+      await this.$axios.$post('/posts', this.form);
+      await this.fetchPosts();
+
+      this.$refs.title.focus();
+      this.form.title = '';
+    },
     async fetchPosts() {
       this.posts = await this.$axios.$get('/posts');
     },
